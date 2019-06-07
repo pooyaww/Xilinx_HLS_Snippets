b8146ca5cf74e2acd7086c3e6351d92cb8097577
Puya Amiri <pooya.w@gmail.com>
2019-06-07 13:38:03 +0200

    removing proj directory

--- sum_io/proj_sum_io/solution1/csim/build/obj/sum_io.d
+++
@@ -1,3 +0,0 @@
-obj/sum_io.o: ../../../../sum_io.c \
-  /opt/Xilinx/Vivado/2018.3/common/technology/autopilot/etc/autopilot_ssdm_op.h \
-  ../../../../sum_io.h
--- sum_io/proj_sum_io/solution1/csim/build/obj/sum_io_test.d
+++
@@ -1,3 +0,0 @@
-obj/sum_io_test.o: ../../../../sum_io_test.c \
-  /opt/Xilinx/Vivado/2018.3/common/technology/autopilot/etc/autopilot_ssdm_op.h \
-  ../../../../sum_io.h
