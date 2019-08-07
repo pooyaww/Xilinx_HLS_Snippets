4af59ad49c5bf40507b9c880922ae766f5336095
Puya <pooya.w@gmail.com>
2019-08-07 13:48:43 +0200

    change with template

---
+++ channel/test.cpp
@@ -0,0 +1,13 @@
+/*template <class T>
+void write_channel(hls::stream<T>* stream, T value) {
+    *stream << value;
+}
+
+template <class T>
+T read_channel(hls::stream<T>* stream) {
+    T value;
+    *stream >> value;
+    return value;
+}
+*/
+extern "C" int f() { return 0; }
