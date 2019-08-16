8daf8c61c50521b5b2085ede555997fceba03303
Puya Amiri <pooya.w@gmail.com>
2019-08-16 14:03:29 +0200

    static variabel needed for long arrays

--- channel/dummy_tb.cpp
+++ channel/dummy_tb.cpp
@@ -3,8 +3,8 @@
 #include "channel.h"
 
 int main() {
-    char r_in[16]={0};
-    char r_out[16]{0};
+    static char r_in[16]={0};
+    static char r_out[16]{0};
     hls_top(r_in,r_out);
     printf("\033[1;31m TOP called from testbench\033[0m\n");
     return 0;
