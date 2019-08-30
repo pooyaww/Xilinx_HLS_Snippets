fe00a716d1a9032c9e474c77e6fbd16792feb4b3
Puya Amiri <puya.amiri@dfki.de>
2019-08-30 19:42:56 +0200

    changing TB array initialization

--- channel/dummy_tb.cpp
+++ channel/dummy_tb.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "channel.h"
 
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
+
 int main() {
-    static char r_in [16] = {0};
-    static char r_out[16] = {0};
+    static int r_in [16];
+    static int r_out[16];
+
+    for (size_t i = 0; i < NELEMS(r_in); ++i) {
+        r_in[i]  = 0;
+        r_out[i] = 0;
+    }
+
     hls_top(r_in,r_out);
     printf("\033[1;31m TOP called from testbench\033[0m\n");
     return 0;
 }
-
-
--- harris_corner/dummy_tb.cpp
+++ harris_corner/dummy_tb.cpp
@@ -1,13 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "harris_corner.h"
 
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
+
 int main() {
-    static int r_in[1048576] = {0};
-    static int r_out[1048576] = {0};
+    static int r_in [1048576];
+    static int r_out[1048576];
+
+    for (size_t i = 0; i < NELEMS(r_in); ++i) {
+        r_in[i]  = 0;
+        r_out[i] = 0;
+    }
+
     hls_top(r_in,r_out);
     printf("\033[1;31m TOP called from testbench\033[0m\n");
     return 0;
 }
-
 
